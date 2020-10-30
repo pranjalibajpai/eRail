553b5ee945380a54fc045473368e95efcfedda61
Pranjali Bajpai <2018eeb1243@iitrpr.ac.in>
2020-10-30 17:20:05 +0530

    Book ticket form added

--- book-ticket.php
+++ book-ticket.php
@@ -9,6 +9,8 @@
     else{
         header('Location: index.php');
     }
+
+    include "config/connection.php";
 ?>
 
 <!DOCTYPE html>
@@ -16,6 +18,39 @@
 
 <?php include "template/header-name.php" ?>
 
+<form action="release-train.php" method="POST">
+    <h3 class="heading">Book Ticket</h3> <br>
+    <label>
+    <p class="label-txt">TRAIN NUMBER</p>
+    <input type="number" class="input" min=0 name="train_number" >
+    <div class="line-box">
+        <div class="line"></div>
+    </div>
+    </label>
+    <label>
+    <p class="label-txt">DATE</p>
+    <input type="date" class="input" name="date">
+    <div class="line-box">
+        <div class="line"></div>
+    </div>
+    </label>
+    <label>
+    <p class="label-txt">COACH&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+      <input type="radio" name="coach" value="ac">&nbsp&nbsp&nbspAC</input>&nbsp&nbsp&nbsp
+      <input type="radio" name="coach" value="sleeper">&nbsp&nbsp&nbspSLEEPER</input></p>
+    </label><br>
+    <label>
+    <p class="label-txt">NUMBER OF PASSENGERS</p>
+    <input type="number" class="input" min=1 name="num_passengers">
+    <div class="line-box">
+        <div class="line"></div>
+    </div>
+    </label>
+
+    <br><br><br>
+    <a href="user.php" class="register">Back</a>
+    <button type="submit" name="book" value="submit">Book</button>
+</form>
 
 <?php include "template/footer.php" ?>
 
