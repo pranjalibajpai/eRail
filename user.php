2ab7125f297eec1d1a06c9792bc24ab5785bfd50
Pranjali Bajpai <2018eeb1243@iitrpr.ac.in>
2020-10-30 15:38:57 +0530

    User page setup

--- user.php
+++ user.php
@@ -9,13 +9,26 @@
     else{
         header('Location: index.php');
     }
-
+    $welcome_name = $_SESSION['username'] ?? 'Guest';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <?php include "template/header-name.php" ?>
+
+<form> 
+    <h3 class="heading">Welcome <?php echo $welcome_name ?></h3> <br>
+    <div class="line-box">
+      <div class="line"></div>
+    </div><br><br>
+    <a href="book-ticket.php" class="register"> Book New Ticket </a><br><br><br>
+    <a href="view-released-trains.php" class="register">View Available Trains</a><br><br><br>
+    <a href="view-user-booking.php" class="register">View Your Previous Bookings</a><br><br><br>
+    <div class="line-box">
+      <div class="line"></div>
+    </div>
+</form>
 
 <?php include "template/footer.php" ?>
 
