8ef326254454454200f421aa2375d88123438afa
Pranjali Bajpai <2018eeb1243@iitrpr.ac.in>
2020-10-31 17:32:22 +0530

    Booking portal setup done

--- book-ticket.php
+++ book-ticket.php
@@ -3,14 +3,50 @@
     //To prevent user to access the page without login
     if(isset($_SESSION['username'])){
         if($_SESSION['username'] == 'admin1'){
-          header('Location: admin-login.php');
+          header('Location: admin-page.php');
         }
     }
     else{
         header('Location: index.php');
     }
-
     include "config/connection.php";
+    $errors = array('train_number' => '', 'date' => '', 'num_passengers' => '', 'validate' => '');
+    $train_number = $date = $coach = $num_passengers = ''; 
+
+   
+    if(isset($_POST['next'])){
+        $train_number = $_POST['train_number'];
+        $date = $_POST['date'];
+        $coach = $_POST['coach'];
+        $num_passengers = $_POST['num_passengers'];
+
+        if(empty($train_number)){
+			$errors['train_number'] = 'Train Number is required';
+        }
+        if(empty($date)){
+			$errors['date'] = 'Date is required';
+        }
+        if(empty($num_passengers)){
+			$errors['num_passengers'] = 'Enter Number Of Passengers';
+        }
+
+        // CHECK VALIDITY OF TRAIN NUMBER & DATE
+        $train_number = $conn->real_escape_string($train_number);
+        $date = $conn->real_escape_string($date);
+        $query = "CALL check_valid_train('$train_number', '$date')";
+        if ($conn->query($query) === FALSE) {
+            $errors['validate'] = $conn->error;
+        }
+
+        // IF NO ERRORS ENTER DETAILS OF PASSENGER
+        if(! array_filter($errors)){
+            $_SESSION['train_number'] = $train_number;
+            $_SESSION['date'] = $date;
+            $_SESSION['coach'] = $coach;
+            $_SESSION['num_passengers'] = $num_passengers;
+            header('Location: passenger-details.php');
+        }
+    }   
 ?>
 
 <!DOCTYPE html>
@@ -18,38 +54,41 @@
 
 <?php include "template/header-name.php" ?>
 
-<form action="release-train.php" method="POST">
+<form action="book-ticket.php" method="POST">
     <h3 class="heading">Book Ticket</h3> <br>
     <label>
     <p class="label-txt">TRAIN NUMBER</p>
-    <input type="number" class="input" min=0 name="train_number" >
+    <input type="number" class="input" min=0 name="train_number" value="<?php echo htmlspecialchars($train_number) ?>">
     <div class="line-box">
         <div class="line"></div>
     </div>
+    <p class= "bg-danger text-white"><?php echo htmlspecialchars($errors['train_number'])?></p>
     </label>
     <label>
     <p class="label-txt">DATE</p>
-    <input type="date" class="input" name="date">
+    <input type="date" class="input" name="date" value="<?php echo htmlspecialchars($date) ?>">
     <div class="line-box">
         <div class="line"></div>
     </div>
+    <p class= "bg-danger text-white"><?php echo htmlspecialchars($errors['date'])?></p>
     </label>
+    <p class= "bg-danger text-white"><?php echo htmlspecialchars($errors['validate'])?></p>
     <label>
     <p class="label-txt">COACH&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-      <input type="radio" name="coach" value="ac">&nbsp&nbsp&nbspAC</input>&nbsp&nbsp&nbsp
-      <input type="radio" name="coach" value="sleeper">&nbsp&nbsp&nbspSLEEPER</input></p>
-    </label><br>
+      <input type="radio" name="coach" value="ac" checked>&nbsp&nbsp&nbspAC</input>&nbsp&nbsp&nbsp
+      <input type="radio" name="coach" value="sleeper">&nbsp&nbsp&nbspSLEEPER</input></p><br>
+    </label>
     <label>
     <p class="label-txt">NUMBER OF PASSENGERS</p>
-    <input type="number" class="input" min=1 name="num_passengers">
+    <input type="number" class="input" min=1 name="num_passengers" value="<?php echo htmlspecialchars($num_passengers) ?>">
     <div class="line-box">
         <div class="line"></div>
     </div>
+    <p class= "bg-danger text-white"><?php echo htmlspecialchars($errors['num_passengers'])?></p>
     </label>
-
-    <br><br><br>
+    <br>
     <a href="user.php" class="register">Back</a>
-    <button type="submit" name="book" value="submit">Book</button>
+    <button type="submit" name="next" value="submit">Next</button>
 </form>
 
 <?php include "template/footer.php" ?>
---
+++ passenger-details.php
@@ -0,0 +1,94 @@
+<?php
+    session_start();
+    //To prevent user to access the page without login
+    if(isset($_SESSION['username'])){
+        if($_SESSION['username'] == 'admin1'){
+          header('Location: admin-login.php');
+        }
+    }
+    else{
+        header('Location: index.php');
+    }
+
+    include "config/connection.php";
+    $num_passengers = $_SESSION['num_passengers'];
+    $errors = array('validate' => '');
+    $name = $age = $gender = [];
+    for($i = 0; $i < $num_passengers; $i++){
+      $name[$i] = $age[$i] = $gender[$i] = '';
+    }
+   
+    if(isset($_POST['check'])){
+      $name = $_POST['name'];
+      $age = $_POST['age'];
+      $gender = $_POST['gender'];
+      
+      if(in_array('', $name, true) || in_array('', $age, true) || in_array('', $gender, true)){
+        $errors['validate'] = 'Please fill details of all the passengers!';
+      }
+
+      //IF NO ERRORS THEN CHECK SEATS ARE AVAILABLE IN DESIRED COACH
+      if(! array_filter($errors)){
+        $_SESSION['name'] = $name;  
+        $_SESSION['age'] = $age;
+        $_SESSION['gender'] = $gender;
+        
+        // TODO - CHECK SEATS ARE AVAILABLE***
+        // IF AVAILABLE THEN REDIRECT GET TICKET ELSE FAILURE PAGE
+        // THEN REDIRECT TO USER PAGE.
+        $available = FALSE;
+        if($available){
+          header('Location: get-ticket.php');
+        }
+        else{
+          header('Location: not-available.php');
+        }
+      }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "template/header-name.php" ?>
+
+<form method="post" action="passenger-details.php">
+  <h3> Enter Details Of Passengers</h3><br>
+  <table>
+    <tr> 
+        <th></th>
+        <th>Name</th> 
+        <th>Age</th> 
+        <th>Gender</th> 
+    </tr> 
+  <?php for($i = 0; $i < $num_passengers; $i++){ ?>
+   <tr>
+   <td>
+   <?php echo $i+1 ?>&nbsp&nbsp&nbsp
+   </td>
+   <td>
+	<input type="text" name="name[]" placeholder="Enter name" value = "<?php echo $name[$i] ?>">&nbsp&nbsp&nbsp
+	</td>
+	<td>
+	<input type="number" name="age[]" placeholder="Enter Age" value = "<?php echo $age[$i] ?>">&nbsp&nbsp&nbsp
+	</td>
+	<td>
+	<select name="gender[]">
+    <option value="Female" <?php echo (isset($gender[$i]) && $gender[$i] === 'Female') ? 'selected' : ''; ?>>Female</option>
+    <option value="Male" <?php echo (isset($gender[$i]) && $gender[$i] === 'Male') ? 'selected' : ''; ?>>Male</option>
+	</select>
+	</td>
+   </tr>
+  <?php } ?>
+  </table>
+    <br><br>
+
+  <p class= "bg-danger text-white"><?php echo htmlspecialchars($errors['validate'])?></p>
+
+  <a href="book-ticket.php" class="register">Back</a>
+  <button type="submit" name="check" value="submit">Check Availability</button>
+ </form>
+
+<?php include "template/footer.php" ?>
+
+</html>
