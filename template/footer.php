ada203e979fa87b5618ec4af6203b27568f056b0
Pranjali Bajpai <2018eeb1243@iitrpr.ac.in>
2020-11-12 23:09:25 +0530

    chore: update footer

--- template/footer.php
+++ template/footer.php
@@ -1,6 +1,7 @@
 <footer>
   <div class="footer bg-light">
-    <p>© Copyright 2020 Made by Pranjali Bajpai & Gazal Arora</p>
+    <p>© Copyright 2020</p>
+    <a href = "https://github.com/pranjalibajpai/railway-reservation-system" style = "float: right;">Github Repo</a>
   </div>
 </footer>
 </body>
