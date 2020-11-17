5f4146d6cd6d63c11265538de5f56edda5fe6aee
Pranjali Bajpai <2018eeb1243@iitrpr.ac.in>
2020-11-17 13:00:45 +0530

    feat: add pnr details nvabar link

---
+++ find-ticket.php
@@ -0,0 +1,47 @@
+<?php
+    session_start();
+    include "config/connection.php";
+    $errors = array('pnr' => '');
+    $pnr = '';
+    if(isset($_POST['submit'])){
+        $pnr = $_POST['pnr'];
+        $pnr = $conn->real_escape_string($pnr);
+
+        //CHECK VALID PNR
+        $query1 = "CALL check_valid_pnr('$pnr');";
+        if ($conn->query($query1) === FALSE) {
+            $errors['pnr'] = $conn->error;
+        }
+
+        if(! array_filter($errors)){
+            $_SESSION['view_pnr'] = $pnr;
+            $conn->close();
+            header('Location: view-pnr-details.php');
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "template/header.php" ?>
+
+<div style="margin-top:200px;">
+    <form style="padding:50px;" action="find-ticket.php" method=POST>
+    
+        <label>
+            <p class="label-txt">ENTER YOUR UNIQUE 10-DIGIT PNR NUMBER</p>
+            <input type="text" class="input" name="pnr" maxlength=12> 
+            <div class="line-box">
+            <div class="line"></div>
+            </div>
+            <p class= "bg-danger text-white"><?php echo htmlspecialchars($errors['pnr'])?></p>
+        </label>
+        <a href="index.php" class="register">Back</a>
+        <button type="submit" name="submit" value="submit">Find Ticket</button>
+    </form>
+</div>
+
+<?php include "template/footer.php" ?>
+
+</html>
---
+++ view-pnr-details.php
@@ -0,0 +1,108 @@
+<?php
+    session_start();
+
+    include "config/connection.php";
+    $query1 = "SELECT * FROM passenger WHERE pnr_no = '".$_SESSION['view_pnr']."' ";
+    if($conn->query($query1) == FALSE){
+        echo $conn->error;
+    }
+    $result1 = $conn->query($query1);
+    $query2 = "SELECT * FROM ticket WHERE pnr_no = '".$_SESSION['view_pnr']."' ";
+    if($conn->query($query2) == FALSE){
+        echo $conn->error;
+    }
+    $result2 = $conn->query($query2);
+    $conn->close(); 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "template/header.php" ?>
+<style> 
+    table { 
+        width: 90%;
+        margin: 0 auto; 
+        font-size: large; 
+        border: 2px solid rgb(120, 120, 120); 
+    }
+    td { 
+        background-color: #E4F5D4; 
+        border: 2px solid rgb(120, 120, 120); 
+    } 
+
+    th, td { 
+        font-weight: bold; 
+        border: 2px dotted rgb(120, 120, 120); 
+        padding: 10px; 
+        text-align: left; 
+    } 
+    td { 
+        font-weight: lighter; 
+    } 
+</style> 
+<div style="margin-top:100px;">
+<form style = "width: 60%;">
+
+    <h5>Booking Details </h5>
+    <table>
+        <tr>
+            <td><h5> PNR NUMBER </h5></td>
+            <td><h5><?php echo $_SESSION['view_pnr'] ?></h5></td>
+        </tr>
+        <?php
+            $row = $result2->fetch_object();
+        ?>
+        <tr>
+            <td><h5> Coach Type</h5></td>
+            <td><h5><?php echo $row->coach ?></h5></td>
+        </tr>
+        <tr>
+            <td><h5> Train Number</h5></td>
+            <td><h5><?php echo $row->t_number ?></h5></td>
+        </tr>
+        <tr>
+            <td><h5> Date Of Journey</h5></td>
+            <td><h5><?php echo $row->t_date ?></h5></td>
+        </tr>
+        <tr>
+            <td><h5> Booked By</h5></td>
+            <td><h5><?php echo $row->booked_by ?></h5></td>
+        </tr>
+    </table><br><br>
+    <h5> Passenger Details </h5>
+    <section>
+        <table> 
+            <tr> 
+                <th>Name</th> 
+                <th>Berth Number</th> 
+                <th>Berth Type</th> 
+                <th>Coach Number</th> 
+            </tr> 
+            <?php 
+                while($rows=$result1->fetch_assoc()) 
+                { 
+             ?> 
+            <tr> 
+                <td><h5><?php echo $rows['name'];?></h5></td> 
+                <td><h5><?php echo $rows['berth_no'];?></h5></td> 
+                <td><h5><?php echo $rows['berth_type'];?></h5></td> 
+                <td><h5><?php echo $rows['coach_no'];?></h5></td> 
+            </tr> 
+            <?php 
+                } 
+             ?> 
+        </table> 
+    </section> 
+    <br>
+
+    <a href="find-ticket.php" class= "register">Back</a>
+    <button onclick="window.print()">Print Ticket</button>
+    
+</form>
+</div>
+
+<?php include "template/footer.php" ?>
+
+</html>
