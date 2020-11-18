5156c3469249a2cac65898c88dfebd66f8df2b5e
Pranjali Bajpai <2018eeb1243@iitrpr.ac.in>
2020-11-18 11:14:47 +0530

    update button position

--- about.php
+++ about.php
@@ -14,8 +14,9 @@
     <form style="padding:50px;">
         <h3>About Us</h3><br>
         This online portal for railway reservation is developed by Pranjali Bajpai & Gazal Arora as a course project for CS301 Database Management System course at IIT Ropar in the Fall Semester of 2020 under the guidance of <a href="http://cse.iitrpr.ac.in/~gunturi/">Prof.Viswanath Gunturi</a>.
+        <a href="index.php" class="register">Back</a>
     </form>
-    <a href="index.php" class="register">Back</a>
+    
 </div>
 
 <?php include "template/footer.php" ?>
