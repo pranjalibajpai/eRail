c20294a295d1a5fd75cab514a6c967ea381e2b18
Pranjali Bajpai <2018eeb1243@iitrpr.ac.in>
2020-11-18 11:13:40 +0530

    add back button & update header file

--- about.php
+++ about.php
@@ -3,13 +3,19 @@
 <head>
     <title>About Us</title>
 </head>
-<?php include "template/header.php" ?>
+<?php 
+    if(isset($_SESSION['username']))
+        include "template/header-name.php";
+    else
+        include "template/header.php";
+?>
 
 <div style="margin-top:200px;">
     <form style="padding:50px;">
         <h3>About Us</h3><br>
         This online portal for railway reservation is developed by Pranjali Bajpai & Gazal Arora as a course project for CS301 Database Management System course at IIT Ropar in the Fall Semester of 2020 under the guidance of <a href="http://cse.iitrpr.ac.in/~gunturi/">Prof.Viswanath Gunturi</a>.
     </form>
+    <a href="index.php" class="register">Back</a>
 </div>
 
 <?php include "template/footer.php" ?>
--- contact.php
+++ contact.php
@@ -3,7 +3,12 @@
 <head>
     <title>Contact Us</title>
 </head>
-<?php include "template/header.php" ?>
+<?php 
+    if(isset($_SESSION['username']))
+        include "template/header-name.php";
+    else
+        include "template/header.php";
+?>
 
 <div style="margin-top:150px;">
     <form style="padding:50px;">
@@ -17,8 +22,8 @@
         <a href="https://in.linkedin.com/in/gazal-arora-52943b167" class="register"><i class="fab fa-linkedin pr-1"></i>Linkedin</a>
         <a href="mailto:2018csb1090@iitrpr.ac.in" class="register"><i class="fab fa-google pr-1"></i> Gmail</a>
         <a href="https://github.com/Gazal1090"class="register"><i class="fab fa-github pr-1"></i> Github</a>
-
     </form>
+    <a href="index.php" class="register">Back</a>
 </div>
 
 <?php include "template/footer.php" ?>
