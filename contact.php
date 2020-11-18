61f2155bc5895346e6d007ce5614b72812562725
Pranjali Bajpai <2018eeb1243@iitrpr.ac.in>
2020-11-18 11:18:04 +0530

    style: button

--- contact.php
+++ contact.php
@@ -16,14 +16,15 @@
         <a href="https://in.linkedin.com/in/pranjali-bajpai" class="register"><i class="fab fa-linkedin pr-1"></i>Linkedin</a>
         <a href="mailto:2018eeb1243@iitrpr.ac.in" class="register"><i class="fab fa-google pr-1"></i> Gmail</a>
         <a href="https://github.com/pranjalibajpai"class="register"><i class="fab fa-github pr-1"></i> Github</a>
-    </form>
-    <form style="padding:50px;">
+        <br><br><br>
         <h3>Gazal Arora</h3><br>
         <a href="https://in.linkedin.com/in/gazal-arora-52943b167" class="register"><i class="fab fa-linkedin pr-1"></i>Linkedin</a>
         <a href="mailto:2018csb1090@iitrpr.ac.in" class="register"><i class="fab fa-google pr-1"></i> Gmail</a>
         <a href="https://github.com/Gazal1090"class="register"><i class="fab fa-github pr-1"></i> Github</a>
+        <br><br><br>
+        <a href="index.php" class="register">Back</a>
     </form>
-    <a href="index.php" class="register">Back</a>
+    
 </div>
 
 <?php include "template/footer.php" ?>
